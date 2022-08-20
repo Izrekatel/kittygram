c1de547e91c031ed3f9cca77fcd3a265659ba60f
Izrekatel <gelrudboris@gmail.com>
2022-08-20 13:47:20 +0500

    apiview

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 
-from cats.views import APICat, APICatDetail
+from cats.views import APICat, APICatDetail, CatList, CatDetail
 
 urlpatterns = [
    path('cats/', APICat.as_view()),
    path('cats/<int:pk>/', APICatDetail.as_view()),
+   path('cats-generic/', CatList.as_view()),
+   path('cats-generic/<int:pk>/', CatDetail.as_view()),
 ]
 
 
